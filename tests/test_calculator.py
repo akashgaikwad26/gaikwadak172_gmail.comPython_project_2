df19fa3b21556334269ebb6bde6d1b1c94e77543
akashgaikwad26 <122025484+akashgaikwad26@users.noreply.github.com>
2024-12-06 14:44:20 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,17 @@
 # tests/test_calculator.py
 
-
-from app.calculator import add,subtract,multiply,divide
-# from app.calculator import add, subtract, multiply, divide
+import pytest
+from app.calculator import add, subtract, multiply, divide
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
-# def test_subtract():
-#     assert subtract(5, 3) == 2
-#     assert subtract(0, 3) == -3
-#     assert subtract(3, 3) == 0
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 3) == -3
+    assert subtract(3, 3) == 0
 
 def test_multiply():
     assert multiply(2, 3) == 6
